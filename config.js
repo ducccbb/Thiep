f9e64244155d23e9fc6d7a79bd242461c6122a0b
ducccbb <106739191+ducccbb@users.noreply.github.com>
2024-02-09 23:52:12 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const nameGirl = '10a4';
 const giftUrl = 'https://www.tiktok.com/@a4.k52.pxb';
 const eventName = 'Chúc Mừng Năm mới';
 const titleCard = 'Tặng 10a4';
-const contentCard = 'Gửi đến tập thể lớp 10A4: Nhân dịp năm mới Quý Mão 2024, kính chúc toàn thể lớp 10A4 một năm mới An khang thịnh vượng - Vạn sự như ý!';
+const contentCard = 'Gửi đến tập thể lớp 10A4: Nhân dịp năm mới Nhâm Dần 2024, kính chúc toàn thể lớp 10A4 một năm mới An khang thịnh vượng - Vạn sự như ý!';
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
 // Cần hỗ trợ hãy liên hệ: 
 // Mr-Nam http://facebook.com/nam.nodemy
